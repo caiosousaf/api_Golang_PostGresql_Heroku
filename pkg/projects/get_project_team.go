4b28d4b941f4f062ccaf3f9f461d09254433715a
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-06-28 10:11:15 -0300

    "Fix: Function get team of project"

--- pkg/projects/get_project_team.go
+++ pkg/projects/get_project_team.go
@@ -5,22 +5,22 @@ import (
 	"net/http"
 )
 
-type Teams struct {
-	ID_project	uint 	`json:"id_project"`
-	ID_Team		uint	`json:"id_equipe"`
-	NameTeam 	string 	`json:"nameTeam"`
+type Equipes struct {
+	ID_project		  uint 		`json:"id_project"`
+	ID_Equipe         uint   	`json:"id_equipe"`
+    Nome_Equipe       string 	`json:"nome_equipe"`
 }
 
 func (h handler) GetProjectTeam(c * gin.Context){
 	id := c.Param("id")
 
-	var teams []Teams
+	var equipes []Equipes
 	
-	sql := "select eq.nameteam from teams as eq inner join teams as pe on eq.id_project = pe.project_id where eq.id_team = ?"
+	sql := "select eq.nameteam from equipes as eq inner join equipes as pe on eq.id_project = pe.project_id where eq.id_team = ?"
 
-	if teams := h.DB.Raw(sql, id).Scan(&teams); teams.Error != nil{
-		c.AbortWithError(http.StatusNotFound, teams.Error)
+	if equipes := h.DB.Raw(sql, id).Scan(&equipes); equipes.Error != nil{
+		c.AbortWithError(http.StatusNotFound, equipes.Error)
 		return
 	}
-	c.JSON(http.StatusOK, &teams)
+	c.JSON(http.StatusOK, &equipes)
 }
