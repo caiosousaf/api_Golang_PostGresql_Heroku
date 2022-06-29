9633f225633db64c3f9e9ef01189fd459210a5d8
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-06-29 09:51:10 -0300

    "Test tasks"

--- pkg/tasks/add_tasks.go
+++ pkg/tasks/add_tasks.go
@@ -8,9 +8,10 @@ import (
 )
 
 type AddTaskRequestBody struct {
-	Descricao_Task  string 			`json:"descricao_task"`
-	PessoaID  		int				`json:"pessoa_id"`
-	ProjetoID 		int 			`json:"projeto_id"`
+	ID_Task			uint	`json:"id_task"`
+	Descricao_Task  string 	`json:"descricao_task"`
+	PessoaID		int 	`json:"pessoa_id"`
+	ProjetoID		int 	`json:"projeto_id"`
 }
 
 func (h handler) AddTask(c *gin.Context) {
@@ -24,6 +25,7 @@ func (h handler) AddTask(c *gin.Context) {
 
 	var task models.Task
 
+	task.ID_Task = body.ID_Task
 	task.Descricao_Task = body.Descricao_Task
 	task.PessoaID = body.PessoaID
 	task.ProjetoID = body.ProjetoID
--- pkg/tasks/update_task.go
+++ pkg/tasks/update_task.go
@@ -8,7 +8,7 @@ import (
 )
 
 type UpdateTaskRequestBody struct {
-	Descricao_Task  string 	`json:"descricao_task"`
+	Descricao_Task  string 			`json:"descricao_task"`
 	PessoaID  		int				`json:"pessoa_id"`
 	ProjetoID 		int 			`json:"projeto_id"`
 }
