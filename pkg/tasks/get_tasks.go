e0a879b77f60abdf524deca281d243d04f24f31b
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-06-29 10:16:59 -0300

    "Test"

--- pkg/tasks/get_tasks.go
+++ pkg/tasks/get_tasks.go
@@ -7,17 +7,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 type Task struct {
-	ID_Task         uint 	`gorm:"primary_key" json:"id_task"`
-	Descricao_Task  string 	`gorm:"type: varchar(100) not null" json:"descricao_task"`
-	PessoaID  		uint	`json:"id_pessoa"`
-	ProjetoID 		uint 	`json:"id_projeto"`
+	ID_Task uint `json:"id_task"`
+	Descricao_Task string `json:"descricao_task"`
+	PessoaID int `json:"pessoa_id"`
+	Nome_Pessoa string `json:"nome_pessoa"`
+	ProjetoID int `json:"projeto_id"`
+	Nome_Projeto string `json:"nome_projeto"`
 }
 
 func (h handler) GetTasks(c *gin.Context) {
-	var tasks []Task
-
-	if result := h.DB.Find(&tasks); result.Error != nil {
-		c.AbortWithError(http.StatusNotFound, result.Error)
+	var tasks []Tasks
+	sql := "select tk.id_task, tk.descricao_task, tk.pessoa_id, pe.nome_pessoa, tk.projeto_id, pr.nome_projeto from tasks as tk inner join pessoas as pe on tk.pessoa_id = pe.id_pessoa inner join projetos as pr on tk.projeto_id = pr.id_projeto order by id_task"
+	if tasks := h.DB.Raw(sql).Scan(&tasks); tasks.Error != nil {
+		c.AbortWithError(http.StatusNotFound, tasks.Error)
 		return
 	}
 	
