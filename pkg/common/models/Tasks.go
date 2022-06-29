8518553b00d147e2124217829407f7b589918ebb
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-06-29 09:40:07 -0300

    "Fix: Tasks"

--- pkg/common/models/Tasks.go
+++ pkg/common/models/Tasks.go
@@ -2,7 +2,7 @@ package models
 
 type Task struct {
 	ID_Task         uint `gorm:"primary_key" json:"id_task"`
-	Descricao_Task  string `gorm:"type: varchar(100) not null" json:"descricao_task"`
+	Descricao_Task  string `json:"descricao_task"`
 	PessoaID  		int		`json:"id_pessoa"`
 	ProjetoID 		int 	`json:"id_projeto"`
 }
