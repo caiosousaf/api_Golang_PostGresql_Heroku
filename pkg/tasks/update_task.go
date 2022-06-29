338209bcae4bf794af4eb12929d6a13ad68b767e
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-06-29 10:00:56 -0300

    a

--- pkg/tasks/update_task.go
+++ pkg/tasks/update_task.go
@@ -8,6 +8,7 @@ import (
 )
 
 type UpdateTaskRequestBody struct {
+	ID_Task			uint			`json:"id_task"`
 	Descricao_Task  string 			`json:"descricao_task"`
 	PessoaID  		int				`json:"pessoa_id"`
 	ProjetoID 		int 			`json:"projeto_id"`
@@ -30,6 +31,7 @@ func (h handler) UpdateTask(c *gin.Context) {
 		return
 	}
 
+	task.ID_Task = body.ID_Task
 	task.Descricao_Task = body.Descricao_Task
 	task.PessoaID = body.PessoaID
 	task.ProjetoID = body.ProjetoID
