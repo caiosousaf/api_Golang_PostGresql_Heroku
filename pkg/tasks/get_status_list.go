a2377d0161a44e07a8897218c3ccf6eb85761954
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-07-04 10:10:33 -0300

    "Fix: Sql of GetStatusList"

--- pkg/tasks/get_status_list.go
+++ pkg/tasks/get_status_list.go
@@ -14,7 +14,7 @@ type GetStatusList struct {
 func (h handler) GetStatusList(c *gin.Context) {
 	var statuslist []GetStatusList
 
-	if result := h.DB.Raw("select * from projetos order by id_projeto").Scan(&statuslist); result.Error != nil {
+	if result := h.DB.Raw("select status, count(*) from tasks group by status").Scan(&statuslist); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
