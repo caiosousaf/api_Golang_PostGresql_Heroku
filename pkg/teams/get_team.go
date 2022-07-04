e900509ae7906b25f3929f879f1f4c53718586a1
PedroMiguel7 <93842972+PedroMiguel7@users.noreply.github.com>
2022-07-04 15:42:04 -0300

    Update get_team.go

--- pkg/teams/get_team.go
+++ pkg/teams/get_team.go
@@ -7,20 +7,37 @@ import (
 	//"github.com/caiosousaf/api_desafio_BrisaNet/pkg/common/models"
 )
 
+
+type MembrosEquipe struct {
+	ID_Pessoa		int		`json:"id_pessoa"`
+	Nome_Pessoa 	string 	`json:"nome_pessoa"`
+	Funcao_Pessoa 	string 	`json:"funcao_pessoa"`
+}
+
 type equipe struct{
-	Id_equipe 			uint `json:"id_equipe"`
-	Nome_Equipe       string `json:"nome_equipe"`
+	Id_equipe 		uint 				`json:"id_equipe"`
+	Nome_Equipe     string 				`json:"nome_equipe"`
+	Membros			[]MembrosEquipe  	`json:"membros"`
 }
 
 func (h handler) GetTeam(c *gin.Context) {
 	id := c.Param("id")
+	
+	var membros []MembrosEquipe
+	sql := `select eq.nome_equipe, id_equipe,pe.id_pessoa, pe.nome_pessoa, pe.funcao_pessoa from equipes as eq inner join
+	pessoas as pe on eq.id_equipe = pe.equipe_id where eq.id_equipe = ?`
+	if membros := h.DB.Raw(sql, id).Scan(&membros); membros.Error != nil {
+		c.AbortWithError(http.StatusNotFound, membros.Error)
+		return
+	}
+
 
 	var equipe []equipe
-
 	if result := h.DB.First(&equipe, id); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
 	}
 
+	c.JSON(http.StatusOK, &membros)
 	c.JSON(http.StatusOK, &equipe)
 }
