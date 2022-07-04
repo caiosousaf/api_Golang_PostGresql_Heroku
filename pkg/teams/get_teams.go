bac7b644b16c68e5e8ac79ebf315a225e418144e
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-07-04 15:54:12 -0300

    "Delete changes "

--- pkg/teams/get_teams.go
+++ pkg/teams/get_teams.go
@@ -7,14 +7,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Equipe struct {
-    ID_Equipe         uint   `gorm:"primary_key" json:"id_equipe"`
-    Nome_Equipe       string `json:"nome_equipe"`
-	Membros			models.Pessoa
-}
-
 func (h handler) GetTeams(c *gin.Context) {
-	var equipes []Equipe
+	var equipes []models.Equipe
 
 	if result := h.DB.Find(&equipes); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
