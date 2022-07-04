600df725a8b9f70978b5d5a1e0e8706acbc9d6f4
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-07-04 15:56:40 -0300

    "Delect router inactive"

--- pkg/teams/controller.go
+++ pkg/teams/controller.go
@@ -20,7 +20,6 @@ func RegisterRoutes(r *gin.Engine, db *gorm.DB) {
 	routes.GET("/:id", h.GetTeam)
 	routes.GET("/:id/projetos", h.GetTeamProject)
 	routes.GET("/:id/pessoas", h.GetTeamMembers)
-	routes.GET("/membros", h.GetTeamsMembers)
 	routes.PUT("/:id", h.UpdateTeam)
 	routes.DELETE("/:id", h.DeleteTeam)
 }
