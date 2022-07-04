f9a6464a760328579d3994d2fedd4a99d30530d4
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-07-04 15:51:18 -0300

    "Fix: Function GetTeams"

--- pkg/teams/get_teams.go
+++ pkg/teams/get_teams.go
@@ -7,8 +7,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Equipe struct {
+    ID_Equipe         uint   `gorm:"primary_key" json:"id_equipe"`
+    Nome_Equipe       string `json:"nome_equipe"`
+	Membros			models.Pessoa
+}
+
 func (h handler) GetTeams(c *gin.Context) {
-	var equipes []models.Equipe
+	var equipes []Equipe
 
 	if result := h.DB.Find(&equipes); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
