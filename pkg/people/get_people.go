415002a89e900c7866c5961fb9b4716c34c2351f
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-07-11 09:56:56 -0300

    "Fix: Function Get Tasks of Project"

--- pkg/people/get_people.go
+++ pkg/people/get_people.go
@@ -11,7 +11,7 @@ type GetPessoasRequestBody struct {
 	ID_Pessoa 		uint	`json:"id_pessoa"`
     Nome_Pessoa		string 	`json:"nome_pessoa"`
 	Funcao_Pessoa	string 	`json:"funcao_pessoa"`
-	EquipeID		int 	`json:"id_equipe"`
+	EquipeID		int 	`json:"equipe_id"`
 	Data_Contratacao string	`json:"data_contratacao"`
 }
 
--- pkg/projects/get_project_tasks.go
+++ pkg/projects/get_project_tasks.go
@@ -10,7 +10,9 @@ type TasksProjeto struct {
 	ID_Projeto 		uint 	`json:"id_projeto"`
 	Nome_Projeto 	string  `json:"nome_projeto"`
 	Nome_Equipe 	string  `json:"nome_equipe"`
+	ID_Task			string	`json:"id_task"`
 	Descricao_Task 	string  `json:"descricao_task"`
+	Status			string	`json:"status"`
 	Nome_Pessoa 	string  `json:"nome_pessoa"`
 }
 
@@ -18,7 +20,11 @@ func (h handler) GetProjectTasks (c *gin.Context) {
 	var tasks []TasksProjeto
 
 	id := c.Param("id")
-	sql := "select pr.id_projeto, pr.nome_projeto, eq.nome_equipe, tk.descricao_task, pe.nome_pessoa from projetos as pr inner join tasks as tk on pr.id_projeto = tk.projeto_id inner join equipes as eq on pr.equipe_id = eq.id_equipe inner join pessoas as pe on pe.id_pessoa = tk.pessoa_id where id_projeto = ?"
+	sql := `select pr.id_projeto, pr.nome_projeto, eq.nome_equipe,tk.id_task, tk.descricao_task, tk.status, 
+	pe.nome_pessoa from 
+	projetos as pr inner join tasks as tk on pr.id_projeto = tk.projeto_id inner join
+	equipes as eq on pr.equipe_id = eq.id_equipe inner join
+	pessoas as pe on pe.id_pessoa = tk.pessoa_id where id_projeto = ?`
 
 	if tasks := h.DB.Raw(sql, id).Scan(&tasks); tasks.Error != nil {
 		c.AbortWithError(http.StatusNotFound, tasks.Error)
