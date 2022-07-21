c20342c4b2d24d2883613d07ccf5b4c7e02d8a4c
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-07-21 09:54:57 -0300

    "Fix: Get projects"

--- pkg/projects/get_project.go
+++ pkg/projects/get_project.go
@@ -4,14 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/caiosousaf/api_Golang_PostGresql_Heroku/pkg/common/models"
+	//"github.com/caiosousaf/api_Golang_PostGresql_Heroku/pkg/common/models"
 
 )
 
 func (h handler) GetProject(c *gin.Context) {
 	id := c.Param("id")
 
-	var projeto []models.Projeto
+	var projeto []Projects
 
 	if result := h.DB.Raw("select * from projetos where id_projeto = ?", id).Scan(&projeto); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
--- pkg/projects/get_projects.go
+++ pkg/projects/get_projects.go
@@ -2,6 +2,7 @@ package projetos
 
 import (
 	"net/http"
+	"time"
 
 	//"github.com/caiosousaf/api_Golang_PostGresql_Heroku/pkg/common/models"
 	"github.com/gin-gonic/gin"
@@ -15,6 +16,7 @@ type Projects struct {
 	Descricao_Projeto	string		`json:"descricao_projeto"`
 	Data_Criacao		string		`json:"data_criacao"`
 	Data_Conclusao		string		`json:"data_conclusao"`
+	Prazo_Entrega		*time.Time	`json:"prazo_entrega"`
 }
 
 type CountProjects struct {
@@ -30,7 +32,7 @@ func (h handler) GetProjects(c *gin.Context) {
 
 
 	if result := h.DB.Raw(`select pr.id_projeto, pr.nome_projeto, pr.equipe_id, eq.nome_equipe, pr.status, pr.descricao_projeto, 
-	pr.data_criacao, pr.data_conclusao
+	pr.data_criacao, pr.data_conclusao, pr.prazo_entrega
 	from projetos as pr inner join equipes as eq on pr.equipe_id = eq.id_equipe`).Scan(&projetos); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
