764ad6a2d25953f2ef8597580282089aee4e6981
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-07-25 08:53:29 -0300

    "Fix: Router of GetTaskPerson, add more data"

--- pkg/people/get_person_tasks.go
+++ pkg/people/get_person_tasks.go
@@ -2,24 +2,33 @@ package pessoas
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Result struct {
-	Nome_Pessoa 	 string	`json:"nome_pessoa"`
-	Funcao_Pessoa	 string `json:"funcao_pessoa"`
-	Nome_Equipe 	 string	`json:"id_equipe"`
-	Nome_Projeto	 string	`json:"nome_projeto"`
-	Descricao_Task	 string	`json:"descricao_task"`
+type result struct {
+	Nome_Pessoa 		string		`json:"nome_pessoa"`
+	Funcao_Pessoa		string 		`json:"funcao_pessoa"`
+	Nome_Equipe 	 	string		`json:"id_equipe"`
+	Nome_Projeto	 	string		`json:"nome_projeto"`
+	ID_Task			 	int			`json:"id_task"`
+	Descricao_Task	 	string		`json:"descricao_task"`
+	Projeto_ID		 	int			`json:"projeto_id"`		
+	Status			 	string		`json:"status"`
+	Data_Criacao	 	*time.Time	`json:"data_criacao"`
+	Data_Conclusao	 	*time.Time	`json:"data_conclusao"`
+	Prazo_Entrega		*time.Time	`json:"prazo_entrega"`
+	Prioridade			int			`json:"prioridade"`
 }
 
 func (h handler) GetTaskPerson(c *gin.Context) {
 	id := c.Param("id")
-	sql := `SELECT pe.id_pessoa, pe.nome_pessoa, pe.funcao_pessoa, eq.nome_equipe, pr.nome_projeto, tk.descricao_task FROM
+	sql := `SELECT pe.id_pessoa, pe.nome_pessoa, pe.funcao_pessoa, eq.nome_equipe, pr.nome_projeto,tk.id_task, tk.descricao_task, tk.projeto_id,
+	 tk.status, tk.data_criacao, tk.data_conclusao, tk.prazo_entrega, tk.prioridade FROM
 	 pessoas AS pe INNER JOIN equipes AS eq ON pe.equipe_id = eq.id_equipe INNER JOIN projetos AS pr ON pr.equipe_id = eq.id_equipe 
 	 INNER JOIN tasks as tk ON tk.projeto_id = pr.id_projeto AND tk.pessoa_id = pe.id_pessoa WHERE pe.id_pessoa = ?`
-	var result []Result
+	var result []result
 
 	if result := h.DB.Raw(sql, id).Scan(&result); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
