7d28dad1e6a335704a1433eadaf37f94e2978cf9
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-07-26 10:58:38 -0300

    "Added: Validation in tasks. validation pessoas/equipes/projetos"

--- pkg/projects/add_project.go
+++ pkg/projects/add_project.go
@@ -1,55 +1,55 @@
 package projetos
 
 import (
-	"net/http"
-	"time"
 	"github.com/caiosousaf/api_Golang_PostGresql_Heroku/pkg/common/models"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"time"
 )
 
 type AddProjetoRequestBody struct {
-	Nome_Projeto		string 				`gorm:"type: varchar(30) not null" json:"nome_projeto"`
-	Equipe_ID 			int					`json:"equipe_id"`
-	Status				string				`json:"status"`
-	Descricao_Projeto	string				`json:"descricao_projeto"`
-	Prazo				int					`json:"prazo_entrega"`
+	Nome_Projeto      string `gorm:"type: varchar(30) not null" json:"nome_projeto"`
+	Equipe_ID         int    `json:"equipe_id"`
+	Status            string `json:"status"`
+	Descricao_Projeto string `json:"descricao_projeto"`
+	Prazo             int    `json:"prazo_entrega"`
 }
 
 func (h handler) AddProject(c *gin.Context) {
 	body := AddProjetoRequestBody{}
-	
+
 	// getting request's body
 	if err := c.BindJSON(&body); err != nil {
 		c.AbortWithError(http.StatusBadRequest, err)
 		return
 	}
-	
+
 	var t = body.Prazo
-	var data_atual = time.Now() 
-	data_limite := data_atual.AddDate(0,0,t)
-	
+	var data_atual = time.Now()
+	data_limite := data_atual.AddDate(0, 0, t)
+
 	var projeto models.Projeto
-	
-	
+
 	projeto.Nome_Projeto = body.Nome_Projeto
 	projeto.EquipeID = body.Equipe_ID
 	projeto.Status = "Em Andamento"
 	projeto.Descricao_Projeto = body.Descricao_Projeto
 	var count int
-	
+
 	err := c.ShouldBindJSON(&projeto)
+	// Verificando se já existe um projeto com o nome digitado
 	if result := h.DB.Raw("select count(*) from projetos where nome_projeto = ?", body.Nome_Projeto).Scan(&count); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
-	} 
-
-	if(count == 0){
+	}
+	// Se não existir nenhum projeto com esse nome ele cria um novo projeto com sucesso
+	if count == 0 {
 		if result := h.DB.Create(&projeto); result.Error != nil {
 			c.AbortWithError(http.StatusNotFound, result.Error)
 			return
 		}
 		c.JSON(http.StatusCreated, &projeto)
-	} else{
+	} else {
 		c.JSON(400, gin.H{
 			"error": "Cannot create Project. already existing project: " + err.Error(),
 		})
--- pkg/tasks/add_tasks.go
+++ pkg/tasks/add_tasks.go
@@ -1,7 +1,6 @@
 package tasks
 
 import (
-	
 	"net/http"
 	"time"
 
@@ -10,13 +9,13 @@ import (
 )
 
 type AddTaskRequestBody struct {
-	ID_Task			uint			`json:"id_task"`
-	Descricao_Task  string 			`json:"descricao_task"`
-	PessoaID		int 			`json:"pessoa_id"`
-	ProjetoID		int 			`json:"projeto_id"`
-	Status			string			`json:"status"`
-	Prazo			int				`json:"prazo_entrega"`
-	Prioridade		int				`json:"prioridade"`
+	ID_Task        uint   `json:"id_task"`
+	Descricao_Task string `json:"descricao_task"`
+	PessoaID       int    `json:"pessoa_id"`
+	ProjetoID      int    `json:"projeto_id"`
+	Status         string `json:"status"`
+	Prazo          int    `json:"prazo_entrega"`
+	Prioridade     int    `json:"prioridade"`
 }
 
 func (h handler) AddTask(c *gin.Context) {
@@ -29,7 +28,7 @@ func (h handler) AddTask(c *gin.Context) {
 	}
 
 	var task models.Task
-	
+	// Prazo em dias que será acrescentado a data atual para que a data_prazo seja retornada
 	var t = body.Prazo
 	task.ID_Task = body.ID_Task
 	task.Descricao_Task = body.Descricao_Task
@@ -37,14 +36,28 @@ func (h handler) AddTask(c *gin.Context) {
 	task.ProjetoID = body.ProjetoID
 	task.Status = "Em Andamento"
 	task.Prioridade = body.Prioridade
-	
-	
-	var data_atual = time.Now() 
-	data_limite := data_atual.AddDate(0,0,t)
 
-	if result := h.DB.Create(&task); result.Error != nil {
+	var count int
+	var data_atual = time.Now()
+	data_limite := data_atual.AddDate(0, 0, t)
+
+	err := c.ShouldBindJSON(&task)
+	if result := h.DB.Raw(`select count(*) from pessoas as pe inner join projetos as pr on pe.equipe_id = pr.equipe_id where pe.id_pessoa = ? and
+	 pr.id_projeto = ?`, body.PessoaID, body.ProjetoID).Scan(&count); result.Error != nil {
 		c.AbortWithError(http.StatusNotFound, result.Error)
 		return
+	}
+
+	if count > 0 {
+		if result := h.DB.Create(&task); result.Error != nil {
+			c.AbortWithError(http.StatusNotFound, result.Error)
+			return
+		}
+		c.JSON(http.StatusCreated, &task)
+	} else {
+		c.JSON(400, gin.H{
+			"error": "Cannot create Task. person is not on the team for this project: " + err.Error(),
+		})
 	}
 
 	if result := h.DB.Model(&task).Where("id_task = ?", task.ID_Task).Update("prazo_entrega", data_limite.Format("2006-01-02")); result.Error != nil {
@@ -52,6 +65,4 @@ func (h handler) AddTask(c *gin.Context) {
 		return
 	}
 
-
-	c.JSON(http.StatusCreated, &task)
 }
