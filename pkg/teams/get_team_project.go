8b31b49f33c229ff24b4d058aa1fabf03bf6fc32
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-08-04 11:42:29 -0300

    "Added: more return information en route teams/:id/projects GET"

--- pkg/teams/get_team_project.go
+++ pkg/teams/get_team_project.go
@@ -2,14 +2,18 @@ package equipes
 
 import (
 	"net/http"
-
+	"time"
 	"github.com/gin-gonic/gin"
 )
 
 type EquipeProjetos struct {
-	Nome_Equipe  string `json:"nome_equipe"`
-	ID_Projeto   uint   `json:"id_projeto"`
-	Nome_Projeto string `json:"nome_projeto"`
+	Nome_Equipe  		string 		`json:"nome_equipe"`
+	ID_Projeto   		uint   		`json:"id_projeto"`
+	Nome_Projeto 		string 		`json:"nome_projeto"`
+	Status            	string     	`json:"status"`
+    Data_Criacao      	string     	`json:"data_criacao"`
+    Data_Conclusao    	string     	`json:"data_conclusao"`
+    Prazo_Entrega     *time.Time 	`json:"prazo_entrega"`
 }
 
 // @Summary Get Projects of a specific Team
@@ -25,7 +29,9 @@ func (h handler) GetTeamProject(c *gin.Context) {
 	var equipe []EquipeProjetos
 	id := c.Param("id")
 
-	if equipe := h.DB.Raw("select eq.nome_equipe, pr.id_projeto, pr.nome_projeto from equipes as eq inner join projetos as pr on eq.id_equipe = pr.equipe_id where eq.id_equipe = ?", id).Scan(&equipe); equipe.Error != nil {
+	if equipe := h.DB.Raw(`select eq.nome_equipe, pr.id_projeto, pr.nome_projeto, pr.status, pr.descricao_projeto, pr.data_criacao, pr.data_criacao, pr.data_conclusao, pr.prazo_entrega 
+	from equipes as eq 
+	inner join projetos as pr on eq.id_equipe = pr.equipe_id where eq.id_equipe = ?`, id).Scan(&equipe); equipe.Error != nil {
 		c.AbortWithError(http.StatusNotFound, equipe.Error)
 		return
 	}
