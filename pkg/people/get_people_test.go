613fa2c5e2a03da9141632a2a6a0e7e42b0a6d88
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-08-23 08:52:13 -0300

    Test circleci

--- pkg/people/get_people_test.go
+++ pkg/people/get_people_test.go
@@ -275,7 +275,7 @@ func Test_handler_DeletePerson(t *testing.T) {
 
 	w := httptest.NewRecorder()
 
-	id := "84"
+	id := "86"
 
 	req, _ := http.NewRequest("DELETE", "/pessoas/"+id, nil)
 
@@ -291,7 +291,7 @@ func Test_handler_DeletePerson(t *testing.T) {
 
 		w := httptest.NewRecorder()
 
-		id := "84"
+		id := "86"
 
 		req, _ := http.NewRequest("DELETE", "/pessoas/"+id, nil)
 
