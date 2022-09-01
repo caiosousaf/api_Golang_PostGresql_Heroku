d812289f727aa5cde1bf145a3704989bfe119810
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-09-01 08:57:07 -0300

    "Fix: Test cors #2"

--- pkg/server/middlewares/auth_middleware.go
+++ pkg/server/middlewares/auth_middleware.go
@@ -9,7 +9,7 @@ func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
         c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
         c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, Accept-Version, Content-MD5, Date, X-api-Version")
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
         if c.Request.Method == "OPTIONS" {
