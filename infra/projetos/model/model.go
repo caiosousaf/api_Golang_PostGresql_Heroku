69a8f7849df71895c5564df8c9d1fed29aa08030
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-09-06 08:07:04 -0300

    :recycle: refactor: Function add_project 2/2

--- infra/projetos/model/model.go
+++ infra/projetos/model/model.go
@@ -5,5 +5,5 @@ type ReqProjeto struct {
 	Nome_Projeto      *string     `json:"nome_projeto"`
 	Equipe_ID         *int        `json:"equipe_id"`
 	Descricao_Projeto *string     `json:"descricao_projeto"`
-	Prazo             *int 		`json:"prazo_entrega"`
+	Prazo             int 		`json:"prazo_entrega"`
 }
--- infra/projetos/postgres/data.go
+++ infra/projetos/postgres/data.go
@@ -3,8 +3,10 @@ package postgres
 import (
 	"database/sql"
 	"fmt"
-	"net/http"
 	modelData "gerenciadorDeProjetos/infra/projetos/model"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,8 +15,11 @@ type DBProjetos struct {
 }
 
 func (postgres *DBProjetos) NovoProjeto(req *modelData.ReqProjeto, c *gin.Context) {
-	sqlStatement := `INSERT INTO projetos(nome_projeto, descricao_projeto, equipe_id, prazo_entrega) VALUES($1, $2 , $3, current_date+$4 );`
-	_, err := postgres.DB.Exec(sqlStatement, req.Nome_Projeto, req.Descricao_Projeto, req.Equipe_ID, req.Prazo)
+	var t = req.Prazo
+	var data_atual = time.Now()
+	data_limite := data_atual.AddDate(0, 0, t)
+	sqlStatement := `INSERT INTO projetos(nome_projeto, descricao_projeto, equipe_id, prazo_entrega) VALUES($1, $2 , $3, $4);`
+	_, err := postgres.DB.Exec(sqlStatement, req.Nome_Projeto, req.Descricao_Projeto, req.Equipe_ID, data_limite)
 	if err != nil {
 		c.AbortWithError(http.StatusNotFound, err)
 	}
