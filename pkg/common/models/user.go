a57b31033643666082d1e9f1cc70f6dfa782cec5
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-09-13 10:37:54 -0300

    :recycle: refactor: Model users

--- pkg/common/models/user.go
+++ pkg/common/models/user.go
@@ -1,17 +1,7 @@
 package models
-
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
 type User struct {
 	ID_Usuario uint           `json:"id_usuario" gorm:"primaryKey"`
 	Nome       string         `json:"nome"`
 	Email      string         `json:"email"`
 	Password   string         `json:"password"`
-	CreatedAt  time.Time      `json:"created"`
-	UpdateAt   time.Time      `json:"updated"`
-	DeletedAt  gorm.DeletedAt `gorm:"index"  json:"deleted"`
 }
