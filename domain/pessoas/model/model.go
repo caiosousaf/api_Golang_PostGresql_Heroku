00b750cbc28d0106b0b636ba989b159c7f4cb7bb
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-09-19 11:29:37 -0300

    :recycle: refactor: Swagger of people for new arquiteture

--- domain/pessoas/model/model.go
+++ domain/pessoas/model/model.go
@@ -3,17 +3,17 @@ package pessoas
 import "time"
 
 type ReqPessoa struct {
-	ID_Pessoa        *int       `json:"id_pessoa"`
-	Nome_Pessoa      *string    `json:"nome_pessoa"`
-	Funcao_Pessoa    *string    `json:"funcao_pessoa"`
-	Equipe_ID        *int       `json:"equipe_id" `
-	Data_Contratacao *time.Time `json:"data_contratacao"`
+	ID_Pessoa        *int       `json:"id_pessoa" example:"4"`
+	Nome_Pessoa      *string    `json:"nome_pessoa" example:"Caio Sousa"`
+	Funcao_Pessoa    *string    `json:"funcao_pessoa" example:"Back-End"`
+	Equipe_ID        *int       `json:"equipe_id" example:"1"`
+	Data_Contratacao *time.Time `json:"data_contratacao" example:"19/09/2022"`
 }
 
 type ReqAtualizarPessoa struct {
-	Nome_Pessoa   *string `json:"nome_pessoa"`
-	Funcao_Pessoa *string `json:"funcao_pessoa"`
-	Equipe_ID     *int    `json:"equipe_id" `
+	Nome_Pessoa   *string `json:"nome_pessoa" example:"Caio Swagger"`
+	Funcao_Pessoa *string `json:"funcao_pessoa" example:"Back-End"`
+	Equipe_ID     *int    `json:"equipe_id" example:"1"`
 }
 
 type ReqMembros struct {
@@ -25,27 +25,27 @@ type ReqMembros struct {
 }
 
 type ReqGetPessoa struct {
-	ID_Pessoa        *uint   `json:"id_pessoa"`
-	Nome_Pessoa      *string `json:"nome_pessoa"`
-	Funcao_Pessoa    *string `json:"funcao_pessoa"`
-	EquipeID         *int    `json:"equipe_id"`
-	Nome_Equipe      *string `json:"nome_equipe"`
-	Data_Contratacao *string `json:"data_contratacao"`
+	ID_Pessoa        *uint   `json:"id_pessoa" example:"4"`
+	Nome_Pessoa      *string `json:"nome_pessoa" example:"Caio Sousa"`
+	Funcao_Pessoa    *string `json:"funcao_pessoa" example:"Back-End"`
+	EquipeID         *int    `json:"equipe_id" example:"4"`
+	Nome_Equipe      *string `json:"nome_equipe" example:"Komanda"`
+	Data_Contratacao *string `json:"data_contratacao" example:"19/09/2022"`
 }
 
 type ReqTarefaPessoa struct {
-	ID_Pessoa      *int       `json:"id_pessoa"`
-	Nome_Pessoa    *string    `json:"nome_pessoa"`
-	Funcao_Pessoa  *string    `json:"funcao_pessoa"`
-	ID_Equipe      *int       `json:"id_equipe"`
-	Nome_Equipe    *string    `json:"nome_equipe"`
-	Nome_Projeto   *string    `json:"nome_projeto"`
-	ID_Task        *int       `json:"id_task"`
-	Descricao_Task *string    `json:"descricao_task"`
-	Projeto_ID     *int       `json:"projeto_id"`
-	Status         *string    `json:"status"`
-	Data_Criacao   *time.Time `json:"data_criacao"`
-	Data_Conclusao *time.Time `json:"data_conclusao"`
-	Prazo_Entrega  *time.Time `json:"prazo_entrega"`
-	Prioridade     *int       `json:"prioridade"`
+	ID_Pessoa      *int       `json:"id_pessoa" example:"4"`
+	Nome_Pessoa    *string    `json:"nome_pessoa" example:"Caio Sousa"`
+	Funcao_Pessoa  *string    `json:"funcao_pessoa" example:"Back-End"`
+	ID_Equipe      *int       `json:"id_equipe" example:"1"`
+	Nome_Equipe    *string    `json:"nome_equipe" example:"Komanda"`
+	Nome_Projeto   *string    `json:"nome_projeto" example:"Casas Bahias"`
+	ID_Task        *int       `json:"id_task" example:"4"`
+	Descricao_Task *string    `json:"descricao_task" example:"Exemplo"`
+	Projeto_ID     *int       `json:"projeto_id" example:"40"`
+	Status         *string    `json:"status" example:"Em Teste"`
+	Data_Criacao   *time.Time `json:"data_criacao" example:"22/09/2022"`
+	Data_Conclusao *time.Time `json:"data_conclusao" example:"22/09/2022"`
+	Prazo_Entrega  *time.Time `json:"prazo_entrega" example:"22/09/2022"`
+	Prioridade     *int       `json:"prioridade" example:"2"`
 }
--- webservice/pessoas/handler.go
+++ webservice/pessoas/handler.go
@@ -10,6 +10,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Security bearerAuth
+// @Summary POST a new Person
+// @Description POST a new Person. For the request to be met, the "nome_pessoa", "funcao_pessoa", "equipe_id" are required. 
+// @Param		Person		body	string		true	"New Person"
+// @Accept json
+// @Produce json
+// @Success 201 {object} modelApresentacao.ReqPessoa "OK"
+// @Failure 400,401 {array} utils.ResError
+// @Tags People
+// @Router /pessoas [post]
 func NovaPessoa(c *gin.Context) {
 	fmt.Println("Tentando cadastrar uma nova pessoa")
 	req := modelApresentacao.ReqPessoa{}
@@ -26,6 +36,16 @@ func NovaPessoa(c *gin.Context) {
 
 }
 
+// @Security bearerAuth
+// Get People
+// @Summary Get All People
+// @Description Returns all registered people and the name of the team they are assigned to
+// @Accept json
+// @Produce json
+// @Success 200 {array} modelApresentacao.ReqGetPessoa "OK"
+// @Failure 404,401 {array} utils.ResError
+// @Tags People
+// @Router /pessoas [get]
 func ListarPessoas(c *gin.Context) {
 	fmt.Println("Tentando Listar todas as pessoas")
 	if pessoas, err := pessoas.ListarPessoas(); err != nil {
@@ -35,6 +55,16 @@ func ListarPessoas(c *gin.Context) {
 	}
 }
 
+// @Security bearerAuth
+// @Summary GET a specific Person
+// @Description GET a specific person
+// @Param		id		path	int		true		"Pessoa_ID"
+// @Accept json
+// @Produce json
+// @Success 200 {array} modelApresentacao.ReqGetPessoa "OK"
+// @Failure 401,404 {array} utils.ResError
+// @Tags People
+// @Router /pessoas/{id} [get]
 func ListarPessoa(c *gin.Context) {
 	id := c.Param("id")
 	fmt.Println("Tentando listar uma pessoa com id especifico")
@@ -45,6 +75,16 @@ func ListarPessoa(c *gin.Context) {
 	}
 }
 
+// @Security bearerAuth
+// @Summary GET All Tasks of a specific Person
+// @Description GET the tasks registered and assigned to a specific person
+// @Param		id		path	int		true		"Pessoa_ID"
+// @Accept json
+// @Produce json
+// @Success 200 {array} modelApresentacao.ReqTarefaPessoa "OK"
+// @Failure 401,404 {array} utils.ResError
+// @Tags People
+// @Router /pessoas/{id}/tasks [get]
 func ListarTarefasPessoa(c *gin.Context) {
 	id := c.Param("id")
 	fmt.Println("Tentando listar tarefas de uma pessoa com id especifico")
@@ -57,6 +97,17 @@ func ListarTarefasPessoa(c *gin.Context) {
 	}
 }
 
+// @Security bearerAuth
+// @Summary PUT Person with ID
+// @Description PUT a specific person. For the request to be met, the "nome_pessoa" and "funcao_pessoa" and "equipe_id" are required
+// @Param        id   				path      	int  	true  	"Pessoa ID"
+// @Param		Pessoa				body		string 	true 	"Pessoa"
+// @Accept json
+// @Produce json
+// @Success 200 {object} modelApresentacao.ReqAtualizarPessoa
+// @Failure 401,400	{array} utils.ResError
+// @Tags People
+// @Router /pessoas/{id} [put]
 func AtualizarPessoa(c *gin.Context) {
 	id := c.Param("id")
 	fmt.Println("Tentando atualizar os dados de uma pessoa")
@@ -76,6 +127,16 @@ func AtualizarPessoa(c *gin.Context) {
 	}
 }
 
+// @Security bearerAuth
+// @Summary DELETE a Person
+// @Description DELETE a person
+// @Param		id		path	int		true		"Pessoa_ID"
+// @Accept json
+// @Produce json
+// @Success 200 {array} utils.ResOk "OK"
+// @Failure 401,400 {array} utils.ResError
+// @Tags People
+// @Router /pessoas/{id} [delete]
 func DeletarPessoa(c *gin.Context) {
 	id := c.Param("id")
 	fmt.Println("Tentando deletar uma pessoa")
