73061933dd1d3a71130a824d2e6bd0d4fa65d502
Caio Sousa <78669953+caiosousaf@users.noreply.github.com>
2022-09-20 08:30:07 -0300

    Delete pkg directory
    Deleted for resolve conflits branch

--- pkg/common/models/login.go
+++
@@ -1,6 +0,0 @@
-package models
-
-type Login struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
--- pkg/common/models/user.go
+++
@@ -1,7 +0,0 @@
-package models
-type User struct {
-	ID_Usuario uint           `json:"id_usuario" gorm:"primaryKey"`
-	Nome       string         `json:"nome"`
-	Email      string         `json:"email"`
-	Password   string         `json:"password"`
-}
