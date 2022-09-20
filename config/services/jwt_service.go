f212e4a2d823afeb7764c4f4c954ac5e809b7139
Caio Sousa <caiosousafernandesferreira@hotmail.com>
2022-09-20 08:56:39 -0300

    Jwt

---
+++ config/services/jwt_service.go
@@ -0,0 +1,58 @@
+package services
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type jwtService struct {
+	secretKey string
+	issure    string
+}
+
+func NewJWTService() *jwtService {
+	return &jwtService{
+		secretKey: "secret-key",
+		issure:    "Gerenciador-Projetos",
+	}
+
+}
+
+type Claim struct {
+	Sum uint `json:"sum"`
+	jwt.StandardClaims
+}
+
+// Token generation function
+func (s *jwtService) GenerateToken(id uint) (string, error) {
+	claim := &Claim{
+		id,
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Hour * 2).Unix(),
+			Issuer:    s.issure,
+			IssuedAt:  time.Now().Unix(),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+
+	t, err := token.SignedString([]byte(s.secretKey))
+	if err != nil {
+		return "", err
+	}
+
+	return t, nil
+}
+
+// Function for validate Token
+func (s *jwtService) ValidateToken(token string) bool {
+	_, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, isValid := t.Method.(*jwt.SigningMethodHMAC); !isValid {
+			return nil, fmt.Errorf("invalid token: %v", token)
+		}
+
+		return []byte(s.secretKey), nil
+	})
+	return err == nil
+}
