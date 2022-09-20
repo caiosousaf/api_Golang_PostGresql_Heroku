079048f34181346c6e36abff0d76b80bf2524641
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-09-20 08:26:33 -0300

    :recycle: refactor: Swagger for heroku

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 	docs.SwaggerInfo.Title = "Gerenciador de Projetos"
 	docs.SwaggerInfo.Description = "REST API Desafio"
 	docs.SwaggerInfo.Version = "1.0"
-	docs.SwaggerInfo.Host = "localhost:8080"
+	docs.SwaggerInfo.Host = "golang-posgre-brisanet.herokuapp.com"
 	docs.SwaggerInfo.BasePath = "/"
 	docs.SwaggerInfo.Schemes = []string{"https"}
 	
