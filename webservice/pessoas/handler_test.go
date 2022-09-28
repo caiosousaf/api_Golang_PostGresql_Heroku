e56a2e40ebc00435a93e643150a82060ee151617
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-09-28 11:45:03 -0300

    Get Person 1/2

--- webservice/pessoas/handler_test.go
+++ webservice/pessoas/handler_test.go
@@ -4,22 +4,18 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-
-	// "gerenciadorDeProjetos/config/server/middlewares"
-	// modelApresentacao "gerenciadorDeProjetos/domain/pessoas/model"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-
 	"gerenciadorDeProjetos/config/server/middlewares"
 	"gerenciadorDeProjetos/config/services"
 	modelApresentacaoLogin "gerenciadorDeProjetos/domain/login/model"
 	modelApresentacao "gerenciadorDeProjetos/domain/pessoas/model"
 	modelData "gerenciadorDeProjetos/infra/login/model"
 	"gerenciadorDeProjetos/webservice/login"
-
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+
 )
 
 func TestGetPerson(t *testing.T) {
@@ -38,16 +34,16 @@ func TestGetPerson(t *testing.T) {
 	r.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
 
-	sla := modelApresentacaoLogin.Login{}
+	pessoa := modelApresentacaoLogin.Login{}
 
-	token, err := services.NewJWTService().GenerateToken(sla.ID_Usuario)
+	token, err := services.NewJWTService().GenerateToken(pessoa.ID_Usuario)
 	if err != nil {
 		return
 	}
 	fmt.Println(token)
+	Router(r.Group("pessoas", middlewares.Auth()) )
 
-	t.Run("BuscaProjetosSucesso", func(t *testing.T) {
-		r.GET("/pessoas", listarPessoas, middlewares.Auth())
+	t.Run("BuscaPessoasSucesso", func(t *testing.T) {
 		
 		req, _ := http.NewRequest("GET", "/pessoas", nil)
 		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
@@ -58,7 +54,25 @@ func TestGetPerson(t *testing.T) {
 		json.Unmarshal(w.Body.Bytes(), &pessoas)
 
 		assert.Equal(t, http.StatusOK, w.Code)
+		
 		assert.NotEmpty(t, pessoas)
+	})
+
+	t.Run("BuscaPessoaSucesso", func(t *testing.T) {
+		//r.GET("/pessoas/:id", listarPessoa)
+		id := "1"
+		
+		req, _ := http.NewRequest("GET", "/pessoas/"+id, nil)
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+
+		var pessoa modelApresentacao.ReqGetPessoa
+		json.Unmarshal(w.Body.Bytes(), &pessoa)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+	
 	})
 
 }
