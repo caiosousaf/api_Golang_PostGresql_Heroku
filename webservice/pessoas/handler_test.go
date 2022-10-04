553061bc87d0f8778b929c0d7bc27fcc4c48ae64
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-10-04 10:10:50 -0300

    :test_tube: test: Created new test. Get People and person sucess and error

--- webservice/pessoas/handler_test.go
+++ webservice/pessoas/handler_test.go
@@ -10,16 +10,19 @@ import (
 	modelApresentacao "gerenciadorDeProjetos/domain/pessoas/model"
 	modelData "gerenciadorDeProjetos/infra/login/model"
 	"gerenciadorDeProjetos/webservice/login"
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestGetPerson(t *testing.T) {
-
+func GetToken() (token string) {
+	var t = &testing.T{}
 	r := gin.Default()
+	r.Use(cors.Default())
 
 	r.POST("/login", login.Login)
 	usuario := modelData.ReqLogin{
@@ -40,11 +43,19 @@ func TestGetPerson(t *testing.T) {
 		return
 	}
 	fmt.Println(token)
-	Router(r.Group("pessoas", middlewares.Auth()))
+	return
+}
+
+func TestGetPeople(t *testing.T) {
+	r := gin.Default()
+	r.GET("/pessoas/", listarPessoas, middlewares.Auth())
+	r.Use(cors.Default())
+	//token := GetToken()
+
 	t.Run("BuscaPessoasSucesso", func(t *testing.T) {
 
 		req, _ := http.NewRequest("GET", "/pessoas/", nil)
-		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+		//req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
 		w := httptest.NewRecorder()
 		r.ServeHTTP(w, req)
 
@@ -56,15 +67,26 @@ func TestGetPerson(t *testing.T) {
 		assert.NotEmpty(t, pessoas)
 	})
 
-	t.Run("BuscaPessoaSucesso", func(t *testing.T) {
-		//r.GET("/pessoas/:id", listarPessoa)
 
-		req, err := http.NewRequest("GET", "/pessoas/1", nil)
+}
+
+func TestGetPerson(t *testing.T) {
+
+	r := gin.Default()
+	r.GET("/pessoas/:id", listarPessoa, middlewares.Auth())
+	r.Use(cors.Default())
+	token := GetToken()
+
+	t.Run("BuscaPessoaSucesso", func(t *testing.T) {
+
+		id := "1"
+		req, err := http.NewRequest("GET", "/pessoas/"+id, nil)
 		if err != nil {
 			fmt.Println(err)
 		}
 
 		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+		w := httptest.NewRecorder()
 
 		r.ServeHTTP(w, req)
 
@@ -73,7 +95,28 @@ func TestGetPerson(t *testing.T) {
 
 		assert.Equal(t, http.StatusOK, w.Code)
 		assert.NotEmpty(t, pessoas)
+	})
 
+	t.Run("BuscaPessoaErroId", func(t *testing.T) {
+
+		id := "2932"
+		req, err := http.NewRequest("GET", "/pessoas/"+id, nil)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		var pessoas modelApresentacao.ReqGetPessoa
+		json.Unmarshal(w.Body.Bytes(), &pessoas)
+
+		assert.Equal(t, http.StatusNotFound, w.Code)
+		assert.Empty(t, pessoas)
 	})
 
 }
+
+
