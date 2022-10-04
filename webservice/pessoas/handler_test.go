53926179d463af308b10047351eb4cd312693a02
caiosousaf <caiosousafernandesferreira@hotmail.com>
2022-10-04 11:10:10 -0300

    :test_tube: test: Created new test. Get Tasks of Person and Add Person. (sucess and errors)

--- webservice/pessoas/handler_test.go
+++ webservice/pessoas/handler_test.go
@@ -9,6 +9,7 @@ import (
 	modelApresentacaoLogin "gerenciadorDeProjetos/domain/login/model"
 	modelApresentacao "gerenciadorDeProjetos/domain/pessoas/model"
 	modelData "gerenciadorDeProjetos/infra/login/model"
+	modelDataPessoa "gerenciadorDeProjetos/infra/pessoas/model"
 	"gerenciadorDeProjetos/webservice/login"
 	"net/http"
 	"net/http/httptest"
@@ -66,8 +67,6 @@ func TestGetPeople(t *testing.T) {
 
 		assert.NotEmpty(t, pessoas)
 	})
-
-
 }
 
 func TestGetPerson(t *testing.T) {
@@ -116,7 +115,183 @@ func TestGetPerson(t *testing.T) {
 		assert.Equal(t, http.StatusNotFound, w.Code)
 		assert.Empty(t, pessoas)
 	})
-
 }
 
+func TestGetTasksPerson(t *testing.T) {
+	r := gin.Default()
+	r.GET("/pessoas/:id/tasks", listarTarefasPessoa, middlewares.Auth())
+	r.Use(cors.Default())
+	token := GetToken()
 
+	t.Run("BuscaTaskPessoaSucesso", func(t *testing.T) {
+
+		id := "1"
+		req, err := http.NewRequest("GET", fmt.Sprintf("/pessoas/%v/tasks", id), nil)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		var tasks []modelApresentacao.ReqTarefaPessoa
+		json.Unmarshal(w.Body.Bytes(), &tasks)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+		assert.NotEmpty(t, tasks)
+	})
+
+	t.Run("BuscaTaskPessoaErroSemTasks", func(t *testing.T) {
+
+		id := "11"
+		req, err := http.NewRequest("GET", fmt.Sprintf("/pessoas/%v/tasks", id), nil)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		var tasks []modelApresentacao.ReqTarefaPessoa
+		json.Unmarshal(w.Body.Bytes(), &tasks)
+
+		assert.Equal(t, http.StatusNoContent, w.Code)
+		assert.Empty(t, tasks)
+	})
+
+	t.Run("BuscaTaskPessoaInexistente", func(t *testing.T) {
+
+		id := "1145"
+		req, err := http.NewRequest("GET", fmt.Sprintf("/pessoas/%v/tasks", id), nil)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+		w := httptest.NewRecorder()
+
+		r.ServeHTTP(w, req)
+
+		var tasks []modelApresentacao.ReqTarefaPessoa
+		json.Unmarshal(w.Body.Bytes(), &tasks)
+
+		assert.Equal(t, http.StatusNotFound, w.Code)
+		assert.Empty(t, tasks)
+	})
+}
+
+func TestAddPerson(t *testing.T) {
+	r := gin.Default()
+	r.POST("/pessoas", novaPessoa, middlewares.Auth())
+	r.Use(cors.Default())
+	token := GetToken()
+
+	t.Run("AdicionarPessoaSucesso", func(t *testing.T) {
+
+		nome_pessoa := "Teste Unitario"
+		funcao_pessoa := "Full-Stack"
+		equipe := 10
+
+		pessoa := modelDataPessoa.ReqPessoa{
+			Nome_Pessoa:   &nome_pessoa,
+			Funcao_Pessoa: &funcao_pessoa,
+			Equipe_ID:     &equipe,
+		}
+
+		jsonValue, _ := json.Marshal(pessoa)
+		req, err := http.NewRequest("POST", "/pessoa/", bytes.NewBuffer(jsonValue))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+
+		var pessoaAdicionada modelApresentacao.ReqPessoa
+		json.Unmarshal(w.Body.Bytes(), &pessoaAdicionada)
+		v := *pessoaAdicionada.ID_Pessoa
+		fmt.Println(v)
+
+		assert.Equal(t, http.StatusCreated, w.Code)
+		assert.NotEmpty(t, pessoa)
+		assert.NotEmpty(t, pessoaAdicionada)
+	})
+
+	t.Run("AdicionarPessoaErroParametros", func(t *testing.T) {
+
+		nome_pessoa := "Teste Unitario"
+		funcao_pessoa := "Full-Stack"
+		equipe := "10"
+
+		type reqPessoaForcaError struct {
+			Nome_Pessoa   *string `json:"nome_pessoa"`
+			Funcao_Pessoa *string `json:"funcao_pessoa"`
+			Equipe_ID     *string    `json:"equipe_id" `
+		}
+
+		pessoa := reqPessoaForcaError{
+			Nome_Pessoa:   &nome_pessoa,
+			Funcao_Pessoa: &funcao_pessoa,
+			Equipe_ID:     &equipe,
+		}
+
+		jsonValue, _ := json.Marshal(pessoa)
+		req, err := http.NewRequest("POST", "/pessoas", bytes.NewBuffer(jsonValue))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+
+		var pessoaAdicionada modelApresentacao.ReqPessoa
+		json.Unmarshal(w.Body.Bytes(), &pessoaAdicionada)
+		v := pessoaAdicionada.ID_Pessoa
+		fmt.Println(v)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		assert.NotEmpty(t, pessoa)
+		assert.Empty(t, pessoaAdicionada)
+	})
+
+	t.Run("AdicionarPessoaSucesso", func(t *testing.T) {
+
+		nome_pessoa := "Teste Unitario"
+		funcao_pessoa := "Full-Stack"
+		equipe := 1052
+
+		pessoa := modelDataPessoa.ReqPessoa{
+			Nome_Pessoa:   &nome_pessoa,
+			Funcao_Pessoa: &funcao_pessoa,
+			Equipe_ID:     &equipe,
+		}
+
+		jsonValue, _ := json.Marshal(pessoa)
+		req, err := http.NewRequest("POST", "/pessoas", bytes.NewBuffer(jsonValue))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %v", token))
+		w := httptest.NewRecorder()
+		r.ServeHTTP(w, req)
+
+		var pessoaAdicionada modelApresentacao.ReqPessoa
+		json.Unmarshal(w.Body.Bytes(), &pessoaAdicionada)
+		v := pessoaAdicionada.ID_Pessoa
+		fmt.Println(v)
+
+		assert.Equal(t, http.StatusBadRequest, w.Code)
+		assert.NotEmpty(t, pessoa)
+		assert.Empty(t, pessoaAdicionada)
+	})
+}
